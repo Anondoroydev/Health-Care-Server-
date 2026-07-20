d739d9462c0490dce72a3213e63b6af34e2641f2
Anondo Ray <ajoysarker553@gmail.com>
2026-07-20 21:32:52 +0600

    Refactor user validation and routes; remove unused helpers and files
    - Simplified user validation schema by removing unnecessary fields and consolidating patient validation.
    - Updated routes to include only user and auth routes, removing commented-out and unused routes.
    - Added catchAsync utility for error handling in async route handlers.
    - Introduced Prisma client initialization in a shared module.
    - Created sendResponse utility for consistent API responses.
    - Removed deprecated file uploader, JWT helpers, and other unused files.
    - Cleaned up configuration settings and removed unused environment variables.
    - Updated TypeScript configuration for consistency and clarity.

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,14 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
-import cron from 'node-cron';
+import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AppointmentService } from './app/modules/Appointment/appointment.service';
-import { PaymentController } from './app/modules/Payment/payment.controller';
+import notFound from './app/middlewares/notFound';
+import config from './config';
 import router from './app/routes';
 
+
 const app: Application = express();
-app.use(cookieParser());
-
-app.post(
-    "/webhook",
-    express.raw({ type: "application/json" }),
-    PaymentController.handleStripeWebhookEvent
-);
-
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: 'http://localhost:3001',
     credentials: true
 }));
 
@@ -26,35 +16,19 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-cron.schedule('*/5 * * * *', () => {
-    try {
-        console.log("🔄 Running unpaid appointment cleanup at", new Date().toISOString());
-        AppointmentService.cancelUnpaidAppointments();
-    } catch (err) {
-        console.error("❌ Cron job error:", err);
-    }
-});
+app.use("/api/v1", router);
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
-        Message: "Ph health care server.."
+        message: "Server is running..",
+        environment: config.node_env,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
     })
 });
 
-app.use('/api/v1', router);
-
 app.use(globalErrorHandler);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
-})
+app.use(notFound);
 
 export default app;
