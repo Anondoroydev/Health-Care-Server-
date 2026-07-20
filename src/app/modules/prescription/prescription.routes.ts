6ba576ae787d68d0249c69fe74b827c11fba90ac
Anondo Ray <ajoysarker553@gmail.com>
2026-07-20 23:38:38 +0600

    final commit

--- src/app/modules/prescription/prescription.routes.ts
+++ src/app/modules/prescription/prescription.routes.ts
@@ -1,14 +1,30 @@
-import { UserRole } from '@prisma/client';
 import express from 'express';
-import auth from '../../middlewares/auth';
 import { PrescriptionController } from './prescription.controller';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
+import validateRequest from '../../middlewares/validateRequest';
+import { PrescriptionValidation } from './prescription.validation';
+
 const router = express.Router();
 
+router.get(
+    '/',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    PrescriptionController.getAllFromDB
+);
+
+router.get(
+    '/my-prescription',
+    auth(UserRole.PATIENT),
+    PrescriptionController.patientPrescription
+)
 
 router.post(
-    "/",
+    '/',
     auth(UserRole.DOCTOR),
-    PrescriptionController.createPrescription
-);
+    validateRequest(PrescriptionValidation.create),
+    PrescriptionController.insertIntoDB
+)
+
 
 export const PrescriptionRoutes = router;
