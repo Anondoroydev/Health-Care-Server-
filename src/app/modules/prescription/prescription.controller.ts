a5a0daa0a0e08f01988ca72964e260efe9b70239
Anondo Ray <ajoysarker553@gmail.com>
2026-07-20 23:38:38 +0600

    final commit

--- src/app/modules/prescription/prescription.controller.ts
+++ src/app/modules/prescription/prescription.controller.ts
@@ -1,21 +1,51 @@
-import { Request, Response } from "express";
-import catchAsync from "../../shared/catchAsync";
-import { IJWTPayload } from "../../types/common";
-import { PrescriptionService } from "./prescription.service";
-import sendResponse from "../../shared/sendResponse";
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+import { PrescriptionService } from './prescription.service';
+import { IAuthUser } from '../../interfaces/common';
+import pick from '../../../shared/pick';
+import { prescriptionFilterableFields } from './prescription.constants';
 
-const createPrescription = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
     const user = req.user;
-    const result = await PrescriptionService.createPrescription(user as IJWTPayload, req.body);
-
+    const result = await PrescriptionService.insertIntoDB(user as IAuthUser, req.body);
     sendResponse(res, {
-        statusCode: 201,
+        statusCode: httpStatus.OK,
         success: true,
-        message: "prescription created successfully!",
-        data: result
-    })
-})
+        message: 'Prescription created successfully',
+        data: result,
+    });
+});
+
+const patientPrescription = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user;
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder'])
+    const result = await PrescriptionService.patientPrescription(user as IAuthUser, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Prescription fetched successfully',
+        meta: result.meta,
+        data: result.data
+    });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, prescriptionFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await PrescriptionService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Prescriptions retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
 
 export const PrescriptionController = {
-    createPrescription
-}
+    insertIntoDB,
+    patientPrescription,
+    getAllFromDB
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,17 +1,24 @@
 import express from 'express';
-import { userRoutes } from '../modules/user/user.routes';
-import { authRoutes } from '../modules/auth/auth.routes';
-import { ScheduleRoutes } from '../modules/schedule/schedule.routes';
-import { doctorScheduleRoutes } from '../modules/doctorSchedule/doctorSchedule.routes';
-import { SpecialtiesRoutes } from '../modules/specialties/specialties.routes';
-import { DoctorRoutes } from '../modules/doctor/doctor.routes';
-import { AdminRoutes } from '../modules/admin/admin.routes';
-import { PatientRoutes } from '../modules/patient/patient.routes';
-import { AppointmentRoutes } from '../modules/appointment/appointment.routes';
-import { PrescriptionRoutes } from '../modules/prescription/prescription.routes';
-
+import { apiLimiter } from '../middlewares/rateLimiter';
+import { AdminRoutes } from '../modules/Admin/admin.routes';
+import { AppointmentRoutes } from '../modules/Appointment/appointment.routes';
+import { AuthRoutes } from '../modules/Auth/auth.routes';
+import { DoctorRoutes } from '../modules/Doctor/doctor.routes';
+import { DoctorScheduleRoutes } from '../modules/DoctorSchedule/doctorSchedule.routes';
+import { MetaRoutes } from '../modules/Meta/meta.routes';
+import { PatientRoutes } from '../modules/Patient/patient.route';
+import { PaymentRoutes } from '../modules/Payment/payment.routes';
+import { PrescriptionRoutes } from '../modules/Prescription/prescription.routes';
+import { ReviewRoutes } from '../modules/Review/review.routes';
+import { ScheduleRoutes } from '../modules/Schedule/schedule.routes';
+import { SpecialtiesRoutes } from '../modules/Specialties/specialties.routes';
+import { userRoutes } from '../modules/User/user.routes';
 
 const router = express.Router();
+
+
+
+router.use(apiLimiter); // Apply to all routes
 
 const moduleRoutes = [
     {
@@ -19,16 +26,12 @@ const moduleRoutes = [
         route: userRoutes
     },
     {
+        path: '/admin',
+        route: AdminRoutes
+    },
+    {
         path: '/auth',
-        route: authRoutes
-    },
-    {
-        path: '/schedule',
-        route: ScheduleRoutes
-    },
-    {
-        path: '/doctor-schedule',
-        route: doctorScheduleRoutes
+        route: AuthRoutes
     },
     {
         path: '/specialties',
@@ -39,21 +42,37 @@ const moduleRoutes = [
         route: DoctorRoutes
     },
     {
-        path: '/admin',
-        route: AdminRoutes
-    },
-    {
         path: '/patient',
         route: PatientRoutes
+    },
+    {
+        path: '/schedule',
+        route: ScheduleRoutes
+    },
+    {
+        path: '/doctor-schedule',
+        route: DoctorScheduleRoutes
     },
     {
         path: '/appointment',
         route: AppointmentRoutes
     },
     {
+        path: '/payment',
+        route: PaymentRoutes
+    },
+    {
         path: '/prescription',
         route: PrescriptionRoutes
     },
+    {
+        path: '/review',
+        route: ReviewRoutes
+    },
+    {
+        path: '/meta',
+        route: MetaRoutes
+    }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route))
